48844db70cb1fa73266e7258dde1f59af7001017
TH a.k.a Abangpa1ace <67219914+Abangpa1ace@users.noreply.github.com>
2020-09-10 03:48:31 +0900

    Update app.js

--- [Project] Weather App(Clone)/app.js
+++ [Project] Weather App(Clone)/app.js
@@ -1,1 +1,29 @@
 "use strict";
+
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+  let locTime = document.querySelector(".location-timezone");
+  let tempDeg = document.querySelector(".temp-degree");
+  let tempDes = document.querySelector(".temp-description");
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      // get Information from 'open weather API'
+      const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=3f28f3fa7b078fc07e23f52d60ca2e45`;
+      fetch(api)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          const { temp } = data.main;
+
+          // Set Dom Elements from API
+          tempDeg.textContent = Math.round(temp - 273);
+        });
+    });
+  }
+});
