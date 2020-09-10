acb7f84149e52a8a9db8b3db397596a3d53fae93
TH a.k.a Abangpa1ace <67219914+Abangpa1ace@users.noreply.github.com>
2020-09-11 03:05:27 +0900

    Update app.js

--- [Project] Weather App(Clone)/app.js
+++ [Project] Weather App(Clone)/app.js
@@ -6,12 +6,13 @@ window.addEventListener("load", () => {
   let locTime = document.querySelector(".location-timezone");
   let tempDeg = document.querySelector(".temp-degree");
   let tempDes = document.querySelector(".temp-description");
+  let degreeSec = document.querySelector(".temperature");
+  const degreeSpan = document.querySelector(".temperature span");
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
-      // get Information from 'open weather API'
       const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=3f28f3fa7b078fc07e23f52d60ca2e45`;
       fetch(api)
         .then((response) => {
@@ -20,10 +21,28 @@ window.addEventListener("load", () => {
         .then((data) => {
           console.log(data);
           const { temp } = data.main;
-
+          const icon = data.weather[0].main;
           // Set Dom Elements from API
-          tempDeg.textContent = Math.round(temp - 273);
+          tempDeg.textContent = Math.round(temp - 273); //Kelvin Temp -> Celsius
+          tempDes.textContent = data.weather[0].description;
+          locTime.textContent = data.name;
+
+          // Set Icon
+          setIcons(icon, document.querySelector(".icon"));
+
+          // Celsius - Farenhiet Formula
+          let Farenheit = Math.round(((temp - 273) * 9) / 5 + 32);
+
+          // Change Celsius - Farenheit
+          degreeSec.addEventListener("click", () => {
+            if (degreeSpan.textContent === "C") {
+              tempDeg.textContent = Farenheit;
+              degreeSpan.textContent = "F";
+            } else {
+              tempDeg.textContent = Math.round(temp - 273);
+              degreeSpan.textContent = "C";
+            }
+          });
         });
     });
   }
-});
