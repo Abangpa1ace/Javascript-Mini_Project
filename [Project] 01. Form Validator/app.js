7fb031e90ef66291cdeb5890151dde838e0b6899
TH a.k.a Abangpa1ace <67219914+Abangpa1ace@users.noreply.github.com>
2020-09-17 02:31:03 +0900

    Add files via upload

---
+++ [Project] 01. Form Validator/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//Selector
+const inputName = document.querySelector('.input-name');
+const inputMail = document.querySelector('.input-mail');
+const inputPwd = document.querySelector('.input-pwd');
+const inputPwd2 = document.querySelector('.input-pwd2');
+
+
+//Event Listener
+
+//Function
