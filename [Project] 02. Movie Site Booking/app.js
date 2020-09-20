c72369bb74e70c771d5d34fb34d3339cc62ba641
TH a.k.a Abangpa1ace <67219914+Abangpa1ace@users.noreply.github.com>
2020-09-21 02:42:17 +0900

    Update app.js

--- [Project] 02. Movie Site Booking/app.js
+++ [Project] 02. Movie Site Booking/app.js
@@ -1,16 +1,21 @@
 'use strict';
 
 //Selector
+const movieFilter = document.querySelector('.movie-filter');
 const movieSeat = document.querySelector('.movie-seat');
+const occupiedSeatArray = [[2,3], [3,5], [4,8], [4,9]];
+const movieCheck = document.querySelector('.movie-check');
 
 //Event Listener
-
-seatMaker(6,9,1,6);
-occupiedSeat([[2,3], [3,5], [4,8], [4,9]]);
+seatMaker(6,9,2,7);
+occupiedSeat(occupiedSeatArray);
+movieSeat.addEventListener('click', (e) => {e.target.classList.toggle('sl')})
+window.addEventListener('load', checkPrice);
+movieFilter.addEventListener('change', checkPrice);
 
 //Function
 
-//좌석 만들기 함수 : 가로개수, 세로개수, 복도 간격띄우기
+//좌석 만들기 함수 : 가로개수, 세로개수, 복도 간격띄우기 - 편의성을 위해 기준을 1,1로 설정
 function seatMaker(x, y, aisle1, aisle2) {
     for (let j=0 ; j<x ; j++) {
         const addSeats = document.createElement('div');
@@ -20,7 +25,7 @@ function seatMaker(x, y, aisle1, aisle2) {
             const addSeat = document.createElement('div');
             addSeat.classList.add('seat', `seat-column${i+1}`);
             addSeats.appendChild(addSeat);
-            if (i===aisle1 || i===aisle2) {
+            if (i===(aisle1-1) || i===(aisle2-1)) {
                 addSeat.style.marginRight = '20px';
             }
         }
@@ -33,8 +38,16 @@ function occupiedSeat(array) {
     for (let i=0 ; i<array.length ; i++) {
         let x = array[i][0];
         let y = array[i][1];
-        let seatRow = document.querySelector(`.seat-row${x}`);
-        let seatColumn = seatRow.querySelector(`.seat-column${y}`);
-        seatColumn.classList.add('oc');
+        let occupied = document.querySelector(`.seat-row${x}`).querySelector(`.seat-column${y}`);
+        occupied.classList.add('oc');
     }
+};
+
+
+function checkPrice() {
+    const cnt = document.querySelector('.cnt');
+    const cntPrice = document.querySelector('.cnt-price');
+    let seats = 1;
+    cnt.innerText = seats;
+    cntPrice.innerText = `$${seats * movieFilter.value}`;
 }
