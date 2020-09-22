56205edc7d26d64976494350785159de392f7d04
TH a.k.a Abangpa1ace <67219914+Abangpa1ace@users.noreply.github.com>
2020-09-23 02:27:58 +0900

    Update app.js

--- [Project] 03. Custom Video Player/app.js
+++ [Project] 03. Custom Video Player/app.js
@@ -1,2 +1,17 @@
 'use strict';
 
+//Selector
+const videoScreen = document.querySelector('.video');
+const btnPlay = document.querySelector('.play');
+const btnStop = document.querySelector('.stop');
+
+//Event Listener
+videoScreen.addEventListener('click', () => {
+    // 비디오플레이
+})
+btnPlay.addEventListener('click', () => {
+    btnPlay.classList.toggle('pause')
+})
+
+//Function
+
