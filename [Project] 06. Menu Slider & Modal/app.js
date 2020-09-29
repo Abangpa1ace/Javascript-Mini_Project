44bd6e02a098f9cd91dd496634ddbb500818f0a7
TH a.k.a Abangpa1ace <67219914+Abangpa1ace@users.noreply.github.com>
2020-09-29 15:00:47 +0900

    Update app.js

--- [Project] 06. Menu Slider & Modal/app.js
+++ [Project] 06. Menu Slider & Modal/app.js
@@ -1,29 +1,44 @@
 'use strict';
 
 //Selectors
+const body = document.querySelector('body')
+const menuSlider = document.getElementById('slider')
+const container = document.getElementById('container')
 const modalBtn = document.querySelector('.btn-modal')
 const meunBtn = document.querySelector('.btn-menu')
-const menuSlider = document.querySelector('#slider')
-const loginDiv = document.querySelector('#login')
+const loginDiv = document.getElementById('login')
+const closeBtn = document.querySelector('.login-close')
 const inputMail = document.querySelector('.input-mail')
 const submitBtn = document.querySelector('.submit-btn')
 
 //Event Listeners
 modalBtn.addEventListener('click', modalEvent)
 meunBtn.addEventListener('click', menuEvent)
+closeBtn.addEventListener('click', () => {
+    const filter = document.querySelector('.filter')
+    body.removeChild(filter)
+    loginDiv.style.opacity = "0";
+})
 submitBtn.addEventListener('click', submitInfo)
 
 //Functions
 function modalEvent() {
     const filter = document.createElement('div')
     filter.classList.add('filter')
-    document.querySelector('body').appendChild(filter);
+    body.appendChild(filter);
+    loginDiv.style.display = "block"
     loginDiv.style.opacity = "1"
 }
 
 function menuEvent() {
-    menuSlider.style.width = "50px"
-    menuSlider.style.height = "100vh"
+    if (body.classList.contains('show-slide')===true) {
+        body.style.transform = "translateX(0px)";
+        body.classList.remove('show-slide');
+    } 
+    else {
+        body.style.transform = "translateX(200px)";
+        body.classList.add('show-slide');
+    }
 }
 
 function submitInfo() {
@@ -36,10 +51,12 @@ function submitInfo() {
     if (emailFormCheck.test(inputMail.value) === false) {
         alert('E-Mail 형식이 올바르지 않습니다!')
     }
-    if (pwdInfo !== pwd2Info) {
+    else if (pwdInfo !== pwd2Info) {
         alert('비밀번호가 동일하지 않습니다!')
     }
-    localStorage.setItem('userInfo', JSON.stringify([nameInfo, mailInfo, pwdInfo]))
-    document.querySelector('body').removeChild(filter)
-    loginDiv.style.opacity = "0";
+    else {  
+        localStorage.setItem('userInfo', JSON.stringify([nameInfo, mailInfo, pwdInfo]))
+        body.removeChild(filter)
+        loginDiv.style.opacity = "0";
+    }
 }
