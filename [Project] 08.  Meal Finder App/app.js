738d06b64540cf51673836b518f2cdefaec55d11
TH a.k.a Abangpa1ace <67219914+Abangpa1ace@users.noreply.github.com>
2020-10-06 03:19:58 +0900

    Update app.js

--- [Project] 08.  Meal Finder App/app.js
+++ [Project] 08.  Meal Finder App/app.js
@@ -8,24 +8,71 @@ const schWord = document.querySelector('.search-word')
 const result = document.querySelector('.result-zone')
 
 //Event Listeners
-
 schBtn.addEventListener('click', showMeals)
+rdmBtn.addEventListener('click', randomMeals)
 
 //Functions
 async function showMeals() {
+    clearResult();
+    result.classList.remove('rdm-mode')
+
     const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search.value}`);
     const data = await res.json();
     const meals = data.meals;
     console.log(meals)
 
-    schWord.innerText = `Search results for '${search.value}':`
-    for (let i=0 ; i<meals.length ; i++) {
-        const meal = document.createElement('div')
-        meal.className = 'meal'
-        meal.innerHTML = `<span>${meals[i].strMeal}</span>`
-        meal.style.backgroundImage = `url('${meals[i].strMealThumb}')`
-        meal.style.backgroundSize = '100%'
-        meal.style.backgroundRepeat = 'no-repeat'
-        result.appendChild(meal)
+    if (search.value === "") {
+        alert('검색어를 입력해주세요!');
+        return;
     }
+    else {
+        schWord.innerText = `Search results for '${search.value}' : `
+        for (let i=0 ; i<meals.length ; i++) {
+            const meal = document.createElement('div')
+            meal.className = 'meal'
+            meal.style.background = `url('${meals[i].strMealThumb}')`
+            meal.style.backgroundSize = '100%'
+            meal.style.backgroundRepeat = 'no-repeat'
+            meal.innerHTML = `<a href="${meals[i].strYoutube}"><span>${meals[i].strMeal}</span></a>`
+            result.appendChild(meal)
+        }
+    }
+}
+
+async function randomMeals() {
+    clearResult();
+    result.classList.add('rdm-mode');
+
+    const res = await fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    const data = await res.json()
+    const rdmeal = data.meals[0];
+    console.log(rdmeal);
+
+    schWord.innerText = `${rdmeal.strMeal}`
+    result.innerHTML = `
+        <img src= ${rdmeal.strMealThumb} class="rdm-img">
+        <div class="rdm-contents">
+            <div class="rdm-src">${rdmeal.strCategory}<br><br>${rdmeal.strArea}</div>
+            <p class="rdm-inst">${rdmeal.strInstructions}</p>
+        </div>
+    `
+
+    const ul = document.createElement('ul')
+    ul.innerHTML = '<p>Ingredient</p>'
+    result.appendChild(ul)
+    for (let i=1 ; i<21 ; i++) {
+        let ing = rdmeal[`strIngredient${i}`]
+        let msr = rdmeal[`strMeasure${i}`]
+        if (ing === null || ing === "") {
+            break;
+        }
+        let li = document.createElement('li')
+        li.innerText = `${ing} - ${msr}`
+        ul.appendChild(li)
+    }
+}
+
+function clearResult() {
+    schWord.innerText = "";
+    result.innerHTML = "";
 }
