b64e0dd494d845bd1c96e37895b34b4278731d96
TH a.k.a Abangpa1ace <67219914+Abangpa1ace@users.noreply.github.com>
2020-10-10 17:04:15 +0900

    Update app.js

--- [Project] 09.  Expense Tracker/app.js
+++ [Project] 09.  Expense Tracker/app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// localStorage.clear()
 //Selectors
 const totalCash = document.querySelector('.total-cash')
 const inCash = document.querySelector('.income')
@@ -11,39 +11,88 @@ const cashInput = document.getElementById('transaction-cash')
 const submitBtn = document.querySelector('.transaction-btn')
 
 //Event Listeners
-// showHistory();
+showHistory();
 transaction.addEventListener('submit', newTransaction)
 
 //Functions
 
-// function showHistory() {
-//     const historyList = JSON.parse(localStorage.getItem('history'))
-//     if (historyList !== null && historyList.length > 0) {
-//         alert('hi')
-//     }
-// }
+function showHistory() {
+    const historyList = JSON.parse(localStorage.getItem('historylist'))
+    if (historyList !== null && historyList.length > 0) {
+        for (let i=0 ; i<historyList.length ; i++) {
+            addHistory(historyList[i]['text'], historyList[i]['cash']);
+        }
+    }
+    cashCount(historyList)
+}
 
 function newTransaction(e) {
     e.preventDefault();
+
     const textVal = textInput.value;
     const cashVal = cashInput.value;
     addHistory(textVal, cashVal)
 
     const historyAll = document.querySelectorAll('.history')
-    const historyList = [...historyAll].map(history => history.querySelector('span').innerText)
-    console.log(historyList)
+    const historyList = [...historyAll].map(function(history) {
+        let obj = new Object();
+        obj['text'] = history.querySelector('p').innerText;
+        obj['cash'] = history.querySelector('span').innerText;
+        return obj;
+    })
+    cashCount(historyList)
+    localStorage.setItem('historylist', JSON.stringify(historyList))
+    
+    textInput.value = "";
+    cashInput.value = "";
 }
 
 function addHistory(text, cash) {
     const history = document.createElement('div');
     history.className = 'history'
     historyArea.appendChild(history);
+    history.innerHTML = `<button class="history-btn">X</button><p>${text}</p><span>${cash}</span>`;
     if (cash >= 0) {
         history.classList.add('plus')
-        history.innerHTML = `<span>${text}</span><span>+${cash}</span>`;
     }
     else {
         history.classList.add('minus')
-        history.innerHTML = `<span>${text}</span><span>${cash}</span>`;
     }
+    const historyBtn = document.querySelectorAll('.history-btn')
+    for (const btn of historyBtn) {
+        btn.addEventListener('click', deleteHistory)
+    }
+}
+
+function deleteHistory(e) {
+    e.target.parentNode.remove()
+    
+    const historyAll = document.querySelectorAll('.history')
+    const historyList = [...historyAll].map(function(history) {
+        let obj = new Object();
+        obj['text'] = history.querySelector('p').innerText;
+        obj['cash'] = history.querySelector('span').innerText;
+        return obj;
+    })
+    cashCount(historyList)
+    localStorage.setItem('historylist', JSON.stringify(historyList))
+}
+
+function cashCount(array) {
+    let inVal = 0.00;
+    let outVal = 0.00;
+    if (array !== null) {
+        for (let i=0 ; i<array.length ; i++) {
+            let cashVal = Number(array[i]['cash'])
+            if (cashVal > 0) {
+                inVal += cashVal
+            }
+            else {
+                outVal+= cashVal
+            }
+        }
+    }
+    totalCash.innerText = `$${(inVal+outVal).toFixed(2)}`
+    inCash.innerText = `$${inVal.toFixed(2)}`
+    outCash.innerText = `$${Math.abs(outVal).toFixed(2)}`
 }
