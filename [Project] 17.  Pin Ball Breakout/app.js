29970aa285fcf46f9ff066a30ceeced88f54b5c5
TH a.k.a Abangpa1ace <67219914+Abangpa1ace@users.noreply.github.com>
2020-10-23 23:17:46 +0900

    Update app.js

--- [Project] 17.  Pin Ball Breakout/app.js
+++ [Project] 17.  Pin Ball Breakout/app.js
@@ -4,7 +4,7 @@
 const canvas = document.getElementById('pin-ball')
 const ctx = canvas.getContext('2d')
 
-const blockColor = "#19a3ff"
+const symbolColor = "#19a3ff"
 let initX = 45;
 let initY = 60;
 const rows = 5;
@@ -12,13 +12,24 @@ const columns = 9;
 const blockWidth = 70;
 const blockHeight = 20;
 const blockGap = 10;
+
+let barX = 365;
+const barY = 580;
+const barWidth = 80;
+const barHeight = 10;
+let isPressed = false;
+
 const ballRadius = 10;
 
 //Event Listeners
-singleBlock(blockColor, 365, 580, 80, 10)
+
+
+
+movingBar();
 drawBlocks(columns, rows)
-drawBall('red', 400, 300, ballRadius)
-window.addEventListener('keydown', movingBar)
+drawBall(symbolColor, 400, 300, ballRadius)
+
+
 
 //Functions
 function singleBlock(color, x, y, w, h) {
@@ -29,7 +40,7 @@ function singleBlock(color, x, y, w, h) {
 function drawBlocks(c, r) {
     for (let j=0 ; j<r ; j++) {
         for (let i=0 ; i<c ; i++) {
-            singleBlock(blockColor, initX, initY, blockWidth, blockHeight)
+            singleBlock(symbolColor, initX, initY, blockWidth, blockHeight)
             initX += blockWidth + blockGap;
         }
         initX = 45;
@@ -46,11 +57,53 @@ function drawBall(color, x, y, r) {
 }
 
 function movingBar() {
-    const key = event.keyCode
-    if (key === 37) {
-        canvas.style.backgroundColor = "red"
-    }
-    else if (key === 39) {
-        canvas.style.backgroundColor = "yellow"
-    }
-} the 
+    ctx.beginPath();
+    ctx.lineWidth = "10"
+    ctx.strokeStyle = symbolColor;
+    ctx.moveTo(barX, barY);
+    ctx.lineTo(barX+barWidth, barY);
+    ctx.stroke();
+    ctx.closePath();
+
+    window.addEventListener('keydown', () => {
+        const key = event.keyCode
+        let isPressed = true;
+        window.addEventListener('keyup', () =>{
+            isPressed = false;
+        });
+
+        if (barX > 0 && isPressed && key === 37) {
+            const movingLeft = setInterval(() => {
+                ctx.clearRect(barX, barY-5, barWidth, 10);
+                barX -= 1;
+                ctx.beginPath();
+                ctx.lineWidth = "10"
+                ctx.strokeStyle = symbolColor;
+                ctx.moveTo(barX, barY);
+                ctx.lineTo(barX+barWidth, barY);
+                ctx.stroke();
+                ctx.closePath();
+                if (barX < 0 || !isPressed) {
+                    clearInterval(movingLeft);
+                }
+            }, 1)
+        }
+
+        if (barX < (800-barWidth) && isPressed && key === 39) {
+            const movingRight = setInterval(() => {
+                ctx.clearRect(barX, barY-5, barWidth, 10);
+                barX += 1;
+                ctx.beginPath();
+                ctx.lineWidth = "10"
+                ctx.strokeStyle = symbolColor;
+                ctx.moveTo(barX, barY);
+                ctx.lineTo(barX+barWidth, barY);
+                ctx.stroke();
+                ctx.closePath();
+                if (barX > (800-barWidth) || !isPressed) {
+                    clearInterval(movingRight);
+                }
+            }, 1)
+        }
+    })
+};
