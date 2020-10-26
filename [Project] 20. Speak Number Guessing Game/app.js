39d427d996b70c1caef3f4f46ed20339766cd333
TH a.k.a Abangpa1ace <67219914+Abangpa1ace@users.noreply.github.com>
2020-10-26 16:48:05 +0900

    Update app.js

--- [Project] 20. Speak Number Guessing Game/app.js
+++ [Project] 20. Speak Number Guessing Game/app.js
@@ -11,9 +11,12 @@ recognition.maxAlternatives = 1;
 
 let randomNum = Math.floor(Math.random()*100)
 console.log(randomNum)
+
+const container = document.getElementById('container')
 const resultCon = document.querySelector('.result-container')
 const result = document.querySelector('.result');
 const resultChk = document.querySelector('.result-check');
+
 let resultValue;
 let isRecognizing = false;
 
@@ -21,34 +24,49 @@ let isRecognizing = false;
 recognition.start();
 recording();
 
+
 //Functions
 function recording() {
+
     isRecognizing = true;
 
     recognition.onspeechend = function() {
         isRecognizing = false;
-        recognition.stop();
         if (!isRecognizing) {
             recognition.onresult = function(event) {
                 resultValue = event.results[0][0].transcript;
                 resultCon.style.display = 'flex';
                 result.innerText = resultValue;
-                checkResult(resultValue)
+                checkValue(resultValue);
         }
     }
 }
+}
 
-function checkResult(val) {
-        if (val < randomNum) {
+function checkValue(val) {
+    if (val == randomNum) {
+        recognition.stop();
+        container.innerHTML = `
+            <h1>Congratulations!</h1>
+            <h3>The answer was ${val}!</h3>
+            <button class="again-btn">Try again</button>`
+            
+            const againBtn = document.querySelector('.again-btn')
+            againBtn.addEventListener('click', () => {
+                location.reload();
+            })
+    }
+
+    else {
+        if(isNaN(val)) {
+            resultChk.innerText = 'It is not a valid number!'
+        }
+        else if (val < randomNum) {
             resultChk.innerText = 'Go Upper'
         }
-
         else if (val > randomNum) {
             resultChk.innerText = 'Go Lower'
         }
-
-        else if (val === randomNum) {
-            resultChk.innerText = 'Correct!'
-        }
+        recording();
     }
 }
